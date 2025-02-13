89001011a9107200eeef19967d2d8eb15935fb75
ashfaq <ashfaqmd007@gmail.com>
2025-02-13 13:03:31 +0530

    fds

---
+++ day/variable.tf
@@ -0,0 +1,21 @@
+variable "ami_id" {
+    description = "ami-id"
+    type = string
+    default = ""
+  
+}
+
+variable "instace_type" {
+    description = "value"
+    type = string
+    default = ""
+  
+}
+
+variable "key" {
+    description = "value"
+    type = string
+    default = ""
+  
+
+}
